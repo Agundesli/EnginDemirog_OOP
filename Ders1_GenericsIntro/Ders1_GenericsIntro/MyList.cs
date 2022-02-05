615549293727c0a2dec08c1435ba4180773f98aa
Abdullah <agundesli06@gmail.com>
2022-02-05 21:46:49 +0300

    Generics

---
+++ Ders1_GenericsIntro/Ders1_GenericsIntro/MyList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders1_GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+        
+        //Constructer
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] temparray = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < temparray.Length; i++)
+            {
+                items[i] = temparray[i];
+            }
+            items[items.Length - 1] = item;
+        }
+    }
+}
---
+++ Ders1_GenericsIntro/Ders1_GenericsIntro/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ders1_GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List,Koleksiyonlar vs olmasa idi kendi listelerimi nasıl olşturabilirim?
+            //Generic Classların çalışacağı türü <> ifadelerinin arasında belirtmek zorundayım.
+            //Çünkü genric classalar birden fazla değişken tipi barındırdığı için biz yazacağımız koda göre
+            //gerenic methoda string mi int mi bool mu belirtmek zorundayız
+            //GenericClass lar çalışacağın özel bir type olduğunu belirtmeni sağlar
+            //ben sadece bir sabit değişken türü verirsem sadece o değş. ile çalışır.
+            //ama ben bunu değş istediğim için <> ifadesini içerisne bit type etıyorum. Alakasız bir type
+            //Bu type daha sonra new ettiğim dizi kullanılırken isteğe bağlı değş. atamamı sağlasın diye.
+            //list arka planında bir array tutuyor.
+            MyList<string> isimler = new MyList<string>();
+        }
+    }
+}
+ 
