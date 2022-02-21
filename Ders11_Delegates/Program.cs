3471bb98c6a8ea3d6f6ac6e03d11890ec39137e8
Abdullah <agundesli06@gmail.com>
2022-02-21 06:57:25 +0300

    Delegates

---
+++ Ders11_Delegates/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ders11_Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
